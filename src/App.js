388f7fada7b771bcd46153a1bf61d998d9735ba1
shyam Prasad <shyamimmadi7@gmail.com>
2023-05-20 23:59:26 +0530

    front design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=6f7a457908b03ede32c3c36af9f19250`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app ">
+      <main className="container warm">
+        <div className="shade">
+          <div className="search-box">
+              <input type="text" className="search-bar" placeholder="Search..." />
+          </div>
+          <div className="content">
+              <div className="top">
+                  <div className="location">
+                    Dallas
+                  </div>
+                  <div className="temp">
+                    65 F
+                  </div>
+                  <div className="description">
+                    clouds
+                  </div>
+              </div>
+              <div className="bottom">
+                  <div className="feels">
+                    <p className="val">70 F</p>
+                    <p>Feels like</p>
+                  </div>
+                  <div className="humidity">
+                    <p  className="val">20%</p>
+                    <p>Humidity</p>
+                  </div>
+                  <div className="winds">
+                    <p  className="val">12 MPH</p>
+                    <p>Wind</p>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </main>
+      
     </div>
   );
 }
